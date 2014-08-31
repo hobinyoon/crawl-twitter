f011a2ef4a7b2ab690dfbe7df9b33d5cec3adb6a
Hobin Yoon <hobinyoon@gmail.com>
2014-08-31 11:33:57 -0400

    crawl follower ids

---
+++ src/main/java/Crawl.java
@@ -0,0 +1,55 @@
+package crawltwitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import twitter4j.IDs;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public final class Crawl {
+	static final long CRAWL_FRIENDS_MAX = 1000;
+
+	public static void main(String[] args) {
+		try {
+			ConfigurationBuilder cb = new ConfigurationBuilder();
+			TwitterCredential tc = new TwitterCredential();
+			cb.setDebugEnabled(true)
+				.setOAuthConsumerKey(tc.consumerKey)
+				.setOAuthConsumerSecret(tc.consumerSecret)
+				.setOAuthAccessToken(tc.token)
+				.setOAuthAccessTokenSecret(tc.secret);
+			Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+			long seed_twitter_id = 1337785291;
+			long cursor = -1;
+			System.out.println("Listing following ids.");
+			List<Long> fids = new ArrayList();
+			{
+				IDs ids;
+				do {
+					//ids = twitter.getFriendsIDs(seed_twitter_id, cursor);
+					ids = twitter.getFollowersIDs(seed_twitter_id, cursor);
+					for (long id : ids.getIDs()) {
+						fids.add(id);
+						if (fids.size() == CRAWL_FRIENDS_MAX)
+							break;
+					}
+					if (fids.size() == CRAWL_FRIENDS_MAX)
+						break;
+				} while ((cursor = ids.getNextCursor()) != 0);
+			}
+
+			for (long id: fids) {
+				System.out.printf("%d ", id);
+			}
+
+			System.out.printf("\n");
+		} catch (TwitterException te) {
+			te.printStackTrace();
+			System.out.println("Failed to get friends' ids: " + te.getMessage());
+			System.exit(-1);
+		}
+	}
+}

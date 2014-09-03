c5f0f3ec5dbe647222ed6de33de6e7d2eba97b6a
Hobin Yoon <hobinyoon@gmail.com>
2014-09-02 23:12:48 -0400

    case sensitive db

--- db/create-db.sql
+++ db/create-db.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE IF NOT EXISTS twitter;
+-- case sensitive: http://stackoverflow.com/questions/4879846/how-to-configure-mysql-to-be-case-sensitive
+CREATE DATABASE IF NOT EXISTS twitter CHARACTER SET utf8 COLLATE utf8_bin;
 
 CREATE TABLE IF NOT EXISTS twitter.followers (
 	id BIGINT NOT NULL,
@@ -9,3 +10,7 @@ CREATE TABLE IF NOT EXISTS twitter.friends (
 	id BIGINT NOT NULL,
 	friends TEXT NOT NULL,
 	PRIMARY KEY (id));
+
+CREATE TABLE IF NOT EXISTS twitter.child_sns_to_crawl (
+	sn varchar(15),
+	PRIMARY KEY (sn));

12c73daa9352a317e3c703bfa00c3a68818ad4d5
Hobin Yoon <hobinyoon@gmail.com>
2014-09-05 15:13:55 -0400

    update

--- db/backup.sh
+++ db/backup.sh
@@ -7,4 +7,4 @@ FN_OUT="/mnt/mdc-data/pgr/twitter/backup/twitter-"$CUR_DATETIME".sql"
 
 time mysqldump -u twitter -ptwitterpass twitter > $FN_OUT
 
-ls -al $FN_OUT
+find /mnt/mdc-data/pgr/twitter/backup -type f -ls | sort -k 10 -r | head -n 5

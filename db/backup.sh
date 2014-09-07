2697c20083c6daf9062b6a5f855383105e6d0935
Hobin Yoon <hobinyoon@gmail.com>
2014-09-07 10:54:05 -0400

    credential. update.

--- db/backup.sh
+++ db/backup.sh
@@ -7,4 +7,6 @@ FN_OUT="/mnt/mdc-data/pgr/twitter/backup/twitter-"$CUR_DATETIME".sql"
 
 time mysqldump -u twitter -ptwitterpass twitter > $FN_OUT
 
-find /mnt/mdc-data/pgr/twitter/backup -type f -ls | sort -k 10 -r | head -n 5
+find /mnt/mdc-data/pgr/twitter/backup -type f -ls | sort -k 11 -r | head -n 5
+
+echo "Strip the twitter credential table before distribution!"
--- db/create-db.sql
+++ db/create-db.sql
@@ -1,6 +1,17 @@
 -- case sensitive: http://stackoverflow.com/questions/4879846/how-to-configure-mysql-to-be-case-sensitive
 CREATE DATABASE IF NOT EXISTS twitter CHARACTER SET utf8 COLLATE utf8_bin;
 
+CREATE TABLE IF NOT EXISTS twitter.credentials (
+	token VARCHAR(100),
+	token_secret VARCHAR(100) NOT NULL,
+	consumer_key VARCHAR(100) NOT NULL,
+	consumer_secret VARCHAR(100) NOT NULL,
+	for_stream BOOL NOT NULL,
+	status CHAR(1),	-- v: valid, i: invalid, null: unknown
+	last_used TIMESTAMP NULL DEFAULT NULL,	-- permit null
+	last_rate_limited TIMESTAMP NULL DEFAULT NULL,	-- permit null
+	PRIMARY KEY (token));
+
 CREATE TABLE IF NOT EXISTS twitter.followers (
 	id BIGINT NOT NULL,
 	followers TEXT NOT NULL,

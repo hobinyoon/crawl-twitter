43dfe42651d291a043323601c6125b3c4a1c336f
Hobin Yoon <hobinyoon@gmail.com>
2014-09-09 12:31:12 -0400

    use StdoutWriter

--- src/main/java/CrawlSeedUsers.java
+++ src/main/java/CrawlSeedUsers.java
@@ -73,22 +73,22 @@ public class CrawlSeedUsers {
 
 			@Override
 			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-				System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+				StdoutWriter.W("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
 			}
 
 			@Override
 			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-				System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+				StdoutWriter.W("Got track limitation notice:" + numberOfLimitedStatuses);
 			}
 
 			@Override
 			public void onScrubGeo(long userId, long upToStatusId) {
-				System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+				StdoutWriter.W("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
 			}
 
 			@Override
 			public void onStallWarning(StallWarning warning) {
-				System.out.println("Got stall warning:" + warning);
+				StdoutWriter.W("Got stall warning:" + warning);
 			}
 
 			@Override

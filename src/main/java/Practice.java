33bdd7ad5b32802a73b3ca1f2c3c07bf1a1ddd5e
Hobin Yoon <hobinyoon@gmail.com>
2014-09-10 15:40:42 -0400

    update

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -7,28 +7,28 @@ import twitter4j.Twitter;
 
 
 public class Practice {
-	static void GetFriendsIDs() throws Exception {
-		final long CRAWL_FRIENDS_MAX = 5000;
-		long seed_twitter_id = 1337785291;
-		long cursor = -1;
-		System.out.println("Getting friends IDs ...");
-		List<Long> fids = new ArrayList();
+	//static void GetFriendsIDs() throws Exception {
+	//	final long CRAWL_FRIENDS_MAX = 5000;
+	//	long seed_twitter_id = 1337785291;
+	//	long cursor = -1;
+	//	System.out.println("Getting friends IDs ...");
+	//	List<Long> fids = new ArrayList();
 
-		Twitter twitter = TwitterHandlePool.GetNext();
-		IDs ids;
-		do {
-			ids = twitter.getFriendsIDs(seed_twitter_id, cursor);
-			//ids = twitter.getFollowersIDs(seed_twitter_id, cursor);
-			for (long id : ids.getIDs()) {
-				fids.add(id);
-				if (fids.size() == CRAWL_FRIENDS_MAX)
-					break;
-			}
-			if (fids.size() == CRAWL_FRIENDS_MAX)
-				break;
-		} while ((cursor = ids.getNextCursor()) != 0);
+	//	Twitter twitter = TwitterHandlePool.GetNext();
+	//	IDs ids;
+	//	do {
+	//		ids = twitter.getFriendsIDs(seed_twitter_id, cursor);
+	//		//ids = twitter.getFollowersIDs(seed_twitter_id, cursor);
+	//		for (long id : ids.getIDs()) {
+	//			fids.add(id);
+	//			if (fids.size() == CRAWL_FRIENDS_MAX)
+	//				break;
+	//		}
+	//		if (fids.size() == CRAWL_FRIENDS_MAX)
+	//			break;
+	//	} while ((cursor = ids.getNextCursor()) != 0);
 
-		for (long id: fids)
-			System.out.printf("%d\n", id);
-	}
+	//	for (long id: fids)
+	//		System.out.printf("%d\n", id);
+	//}
 }

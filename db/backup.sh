e565f8d49811cf46008cf3f3d32f03673b55c81f
Hobin Yoon <hobinyoon@gmail.com>
2014-09-19 10:17:48 -0400

    7z. better compression. can show the uncompressed file size.

--- db/backup.sh
+++ db/backup.sh
@@ -10,7 +10,7 @@ FN_OUT="/mnt/mdc-data/pgr/twitter/backup/twitter-"$CUR_DATETIME".sql"
 
 time /usr/bin/mysqldump --skip-tz-utc -u twitter -ptwitterpass twitter > $FN_OUT
 
-/bin/bzip2 -v9 $FN_OUT
+/usr/bin/7z a -mx=9 $FN_OUT".7z" $FN_OUT
 
 /usr/bin/find /mnt/mdc-data/pgr/twitter/backup -type f -ls | /usr/bin/sort -k 11 -r | /usr/bin/head -n 5
 

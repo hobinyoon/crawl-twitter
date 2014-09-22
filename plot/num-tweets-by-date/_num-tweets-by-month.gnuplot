bfe948a53160f54f057ea6bb00d813d4dac1fbfd
Hobin Yoon <hobinyoon@gmail.com>
2014-09-22 11:39:05 -0400

    add monthly stat

---
+++ plot/num-tweets-by-date/_num-tweets-by-month.gnuplot
@@ -0,0 +1,21 @@
+# Tested with gnuplot 4.6 patchlevel 4
+
+FN_IN = system("echo $FN_IN")
+FN_OUT = system("echo $FN_OUT")
+
+set terminal pdfcairo enhanced size 4.5in, 3in
+set output FN_OUT
+
+set grid lc rgb "#808080"
+set border 3 lc rgb "#808080"
+
+set xdata time
+set timefmt "%Y%m"
+
+set xtics nomirror scale 0.5,0 font ",9" rotate by -30
+set ytics nomirror scale 0.5,0 font ",9"
+set format x "%Y-%m"
+set yrange [0:]
+
+plot \
+FN_IN u 1:2 w linespoints pointsize 0.2 pointtype 7 not
--- plot/num-tweets-by-date/plot.py
+++ plot/num-tweets-by-date/plot.py
@@ -6,17 +6,19 @@ import sys
 import subprocess
 
 
-FN_DATA = "num-tweets-by-date"
-FN_PLOT = "num-tweets-by-date.pdf"
+FN_DATA_BY_DATE = "num-tweets-by-date"
+FN_DATA_BY_MONTH = "num-tweets-by-month"
+FN_PLOT_BY_DATE = "num-tweets-by-date.pdf"
+FN_PLOT_BY_MONTH = "num-tweets-by-month.pdf"
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def _GenNumByDate():
+def _ByDateGenData():
 	conn = mysql.connector.connect(user="twitter", password="twitterpass", host="localhost", database="twitter")
 	cursor = conn.cursor()
 	query = ("select date(created_at) as d, count(*) as cnt from tweets group by d")
 	cursor.execute(query)
-	fo = open(FN_DATA, "w")
+	fo = open(FN_DATA_BY_DATE, "w")
 	for (d1, cnt) in cursor:
 		fo.write("%s %d\n" % (d1, cnt))
 	fo.close()
@@ -24,21 +26,54 @@ def _GenNumByDate():
 	conn.close()
 
 
-def _Plot():
+def _ByDatePlot():
 	env = os.environ.copy()
-	env["FN_IN"] = FN_DATA
-	env["FN_OUT"] = FN_PLOT
-
+	env["FN_IN"] = FN_DATA_BY_DATE
+	env["FN_OUT"] = FN_PLOT_BY_DATE
 	cmd = "gnuplot %s/_num-tweets-by-date.gnuplot" % CUR_DIR
 	if subprocess.call(cmd, shell=True, env=env) != 0:
 		raise RuntimeError("Error running cmd: %s" % cmd)
+	print "  created %s" % FN_PLOT_BY_DATE
 
-	print "  created %s" % FN_PLOT
+
+def _ByDate():
+	_ByDateGenData()
+	_ByDatePlot()
+
+
+def _ByMonthGenData():
+	conn = mysql.connector.connect(user="twitter", password="twitterpass", host="localhost", database="twitter")
+	cursor = conn.cursor()
+	query = ("select year(created_at) * 100 + month(created_at) as d, count(*) as cnt from tweets group by d")
+	cursor.execute(query)
+	fo = open(FN_DATA_BY_MONTH, "w")
+	for (d1, cnt) in cursor:
+		fo.write("%s %d\n" % (d1, cnt))
+	fo.close()
+	cursor.close()
+	conn.close()
+
+
+def _ByMonthPlot():
+	env = os.environ.copy()
+	env["FN_IN"] = FN_DATA_BY_MONTH
+	env["FN_OUT"] = FN_PLOT_BY_MONTH
+
+	cmd = "gnuplot %s/_num-tweets-by-month.gnuplot" % CUR_DIR
+	if subprocess.call(cmd, shell=True, env=env) != 0:
+		raise RuntimeError("Error running cmd: %s" % cmd)
+
+	print "  created %s" % FN_PLOT_BY_MONTH
+
+
+def _ByMonth():
+	_ByMonthGenData()
+	_ByMonthPlot()
 
 
 def main(argv):
-	_GenNumByDate()
-	_Plot()
+	_ByDate()
+	_ByMonth()
 
 
 if __name__ == "__main__":

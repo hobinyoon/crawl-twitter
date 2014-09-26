8561598075b184d247231b79e19492d891e27cf8
Hobin Yoon <hobinyoon@gmail.com>
2014-09-26 12:18:23 -0400

    number of tweets in the map

--- plot/num-tweets-by-locations/_num-tweets-by-loc.gnuplot
+++ plot/num-tweets-by-locations/_num-tweets-by-loc.gnuplot
@@ -8,9 +8,10 @@ set output FN_OUT
 
 set grid lc rgb "#808080"
 set border 3 lc rgb "#808080"
+set xtics nomirror scale 0.5,0 font ",9" autofreq -180,10
+set ytics nomirror scale 0.5,0 font ",9" autofreq -90,10
 
-set xtics nomirror scale 0.5,0 font ",9"
-set ytics nomirror scale 0.5,0 font ",9"
+set xrange[-180:180]
+set yrange[-90:90]
 
-plot \
-FN_IN u 1:2 w points pointsize 0.1 pointtype 7 not
+plot FN_IN u 1:2:($3*0.1) with points pt 6 ps variable t "# of tweets in 1/10th of longi, lati area"
--- plot/num-tweets-by-locations/heatmap/plot.py
+++ plot/num-tweets-by-locations/heatmap/plot.py
@@ -20,15 +20,30 @@ def _ByLocGenData():
 	conn = mysql.connector.connect(user="twitter", password="twitterpass", host="localhost", database="twitter")
 	cursor = conn.cursor()
 	#query = ("select geo_longi, geo_lati from tweets")
-	query = ("select round(geo_longi, 1) as longi, round(geo_lati, 1) as lati, count(*) as cnt from tweets group by longi, lati order by longi, lati")
+	query = ("select round(geo_longi) as longi, round(geo_lati) as lati, count(*) as cnt from tweets group by longi, lati order by longi, lati")
 	cursor.execute(query)
 	print "  execute query: %0.3f sec" % (time.time() - tmr1)
 
-	fo = open(FN_DATA_BY_LOC, "w")
+	m = {}
+	for x in range(-180, 180+1):
+		for y in range(-90, 90+1):
+			m[x*1000 + y] = 0
+
+	global cnt_max
+	cnt_max = 0
 	for (longi, lati, cnt) in cursor:
-		fo.write("%f %f %d\n" % (longi, lati, math.sqrt(cnt)))
+		cnt = math.log(cnt)
+		m[int(longi) * 1000 + int(lati)] = cnt
+		cnt_max = max(cnt, cnt_max)
 	cursor.close()
 	conn.close()
+	print "  cnt_max=%d" % cnt_max
+
+	fo = open(FN_DATA_BY_LOC, "w")
+	for y in range(-90, 90 + 1):
+		for x in range(-180, 180 + 1):
+			fo.write("%d " % m[x*1000 + y])
+		fo.write("\n")
 	fo.close()
 	print "  %0.3f sec" % (time.time() - tmr)
 
@@ -39,6 +54,7 @@ def _ByLocPlot():
 	env = os.environ.copy()
 	env["FN_IN"] = FN_DATA_BY_LOC
 	env["FN_OUT"] = FN_PLOT_BY_LOC
+	env["CB_RANGE_MAX"] = str(cnt_max)
 	cmd = "gnuplot %s/_num-tweets-by-loc.gnuplot" % CUR_DIR
 	if subprocess.call(cmd, shell=True, env=env) != 0:
 		raise RuntimeError("Error running cmd: %s" % cmd)

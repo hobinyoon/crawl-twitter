1638111f56a4da90ec824cffa9e4b1609dffdc28
Hobin Yoon <hobinyoon@gmail.com>
2014-11-19 14:13:06 -0500

    top topics

---
+++ analysis/top-topics/conf.h
@@ -0,0 +1,17 @@
+#ifndef __CONF_H__
+#define __CONF_H__
+
+#include <string>
+
+namespace Conf {
+	extern const std::string fn_dc_coord;
+	extern size_t partial_load;
+	extern int max_output_lines;
+	extern const std::string fn_tweets;
+
+	extern const std::string fn_top_topics;
+
+	void Init(int argc, char* argv[]);
+};
+
+#endif
---
+++ analysis/top-topics/op.cpp
@@ -0,0 +1,209 @@
+#include <fstream>
+#include <set>
+#include <boost/format.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+#include <boost/filesystem.hpp>
+#include "conf.h"
+#include "dc.h"
+#include "op.h"
+#include "util.h"
+
+using namespace std;
+
+namespace Ops {
+	void _ReadStr(ifstream& ifs, string& str) {
+		size_t s;
+		ifs.read((char*)&s, sizeof(s));
+		str.resize(s);
+		ifs.read((char*)&str[0], s);
+	}
+
+	Entry::Entry(long id_,
+			long uid_,
+			const string& created_at_,
+			double geo_lati_,
+			double geo_longi_,
+			const string& youtube_video_id_,
+			const string& topics_)
+		: id(id_), uid(uid_), created_at_str(created_at_),
+		geo_lati(geo_lati_), geo_longi(geo_longi_),
+		youtube_video_id(youtube_video_id_)
+	{
+		created_at = boost::posix_time::time_from_string(created_at_str);
+
+		static const auto sep = boost::is_any_of(" ");
+		boost::split(topics, topics_, sep);
+	}
+
+	Entry::Entry(ifstream& ifs) {
+		ifs.read((char*)&id, sizeof(id));
+		ifs.read((char*)&uid, sizeof(uid));
+
+		_ReadStr(ifs, created_at_str);
+		created_at = boost::posix_time::time_from_string(created_at_str);
+
+		ifs.read((char*)&geo_lati, sizeof(geo_lati));
+		ifs.read((char*)&geo_longi, sizeof(geo_longi));
+		_ReadStr(ifs, youtube_video_id);
+		ifs.read((char*)&youtube_video_uploader, sizeof(youtube_video_uploader));
+
+		size_t topic_cnt;
+		ifs.read((char*)&topic_cnt, sizeof(topic_cnt));
+		for (size_t i = 0; i < topic_cnt; i ++) {
+			string t;
+			_ReadStr(ifs, t);
+			topics.push_back(t);
+		}
+		ifs.read((char*)&type, sizeof(type));
+	}
+
+	static list<Entry*> _entries;
+
+	void _LoadTweetsFromFile() {
+		const string& fn = Conf::fn_tweets;
+		Util::CpuTimer _(str(boost::format("Loading tweets from file %s ...\n") % fn), 2);
+
+		ifstream ifs(fn.c_str(), ios::binary);
+		if (! ifs.is_open())
+			throw runtime_error(str(boost::format("unable to open file %1%") % fn));
+
+		size_t e_size;
+		ifs.read((char*)&e_size, sizeof(e_size));
+		for (size_t i = 0; i < e_size; i ++) {
+			_entries.push_back(new Entry(ifs));
+
+			if (Conf::partial_load > 0 && i == Conf::partial_load)
+				break;
+		}
+		cout << "    _entries.size()=" << _entries.size() << "\n";
+	}
+
+	void _FilterOutWrites() {
+		Util::CpuTimer _("Filter out writes ...\n", 2);
+
+		size_t before = _entries.size();
+
+		map<string, int> vids_cnt;
+		for (auto it = _entries.begin(); it != _entries.end(); ) {
+			int cnt = -1;
+			if (vids_cnt.find((*it)->youtube_video_id) == vids_cnt.end()) {
+				cnt = 1;
+			} else {
+				cnt = vids_cnt[(*it)->youtube_video_id] + 1;
+			}
+			vids_cnt[(*it)->youtube_video_id] = cnt;
+
+			if (cnt == 1) {
+				it = _entries.erase(it);
+			} else {
+				it ++;
+			}
+		}
+
+		cout << boost::format("    _entries.size()= before %d, after %d (%.2f%%)\n")
+			% before % _entries.size()
+			% (100.0 * _entries.size() / before);
+	}
+
+	void Load() {
+		Util::CpuTimer _("Loading tweets ...\n");
+
+		_LoadTweetsFromFile();
+		//_FilterOutWrites();
+	}
+
+	void GenTopTopics() {
+		Util::CpuTimer _("Generate top topics ...\n");
+
+		// count topics only once for the same videos
+		list<Entry*> entries;
+		{
+			set<string> vids;
+			for (auto& e: _entries) {
+				string& vid = e->youtube_video_id;
+				auto it = vids.find(vid);
+				if (it == vids.end()) {
+					entries.push_back(e);
+					vids.insert(vid);
+				}
+			}
+		}
+
+		map<string, int> topic_cnt;
+		for (auto& e: entries) {
+			for (auto& t: e->topics) {
+				auto it = topic_cnt.find(t);
+				if (it == topic_cnt.end()) {
+					topic_cnt[t] = 1;
+				} else {
+					it->second ++;
+				}
+			}
+		}
+
+		map<int, vector<string> > cnt_topic;
+		for (auto tc: topic_cnt) {
+			int cnt = tc.second;
+			const string& t = tc.first;
+
+			auto it = cnt_topic.find(cnt);
+			if (it == cnt_topic.end()) {
+				vector<string> v;
+				v.push_back(t);
+				cnt_topic[cnt] = v;
+			} else {
+				it->second.push_back(t);
+			}
+		}
+
+		const string& fn = Conf::fn_top_topics;
+		{
+			ofstream ofs(fn);
+			if (! ofs.is_open())
+				throw runtime_error(str(boost::format("unable to open file %1%") % fn));
+
+			int i = 0;
+			for (auto it = cnt_topic.rbegin(); it != cnt_topic.rend(); it ++) {
+				int cnt = it->first;
+
+				for (auto& t: it->second) {
+					ofs << t << " " << cnt << "\n";
+
+					i ++;
+					if (i == Conf::max_output_lines)
+						break;
+				}
+
+				if (i == Conf::max_output_lines)
+					break;
+			}
+		}
+		cout << "  Created " << fn << " " << boost::filesystem::file_size(fn) << "\n";
+	}
+
+	void FreeMem() {
+		for (auto e: _entries)
+			delete e;
+	}
+}
+
+
+std::ostream& operator<< (std::ostream& os, const Ops::Entry& op) {
+	os << boost::format("%ld %10ld %s %s %10lf %11lf %s")
+		% op.id
+		% op.uid
+		% op.created_at
+		% op.type
+		% op.geo_lati
+		% op.geo_longi
+		% op.youtube_video_id;
+	return os;
+}
+
+
+std::ostream& operator<< (std::ostream& os, const Ops::Type& opt) {
+	static const char* s[] = {"NA", "W", "R"};
+	os << s[opt];
+	return os;
+}

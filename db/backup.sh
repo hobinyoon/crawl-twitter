8c1adb5f70276eb227dd76800d94ea15be890d40
Hobin Yoon <hobinyoon@gmail.com>
2015-04-28 14:21:24 -0400

    update

--- db/backup.sh
+++ db/backup.sh
@@ -7,13 +7,14 @@ DB_NAME=twitter3
 CUR_DATETIME=`date +%y%m%d-%H%M%S`
 echo $CUR_DATETIME
 
-FN_OUT="/mnt/mdc-data/pgr/twitter/backup/"$DB_NAME"-"$CUR_DATETIME".sql"
+FN_OUT="/mnt/data/twitter/backup/"$DB_NAME"-"$CUR_DATETIME".sql"
 
 time /usr/bin/mysqldump --skip-tz-utc -u twitter -ptwitterpass $DB_NAME > $FN_OUT
 
 /usr/bin/7z a -mx=9 $FN_OUT".7z" $FN_OUT
 \rm $FN_OUT
 
-/usr/bin/find /mnt/mdc-data/pgr/twitter/backup -type f -ls | /usr/bin/sort -k 11 -r | /usr/bin/head -n 5
+/usr/bin/find /mnt/data/twitter/backup -type f -ls | /usr/bin/sort -k 11 -r | /usr/bin/head -n 5
 
-echo "\nStrip the credential table before distribution!"
+echo
+echo "Strip the credential table before distribution!"

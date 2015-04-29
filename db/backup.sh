39d86629b40bec742f1bcfcdb5e887027c6b1b65
Hobin Yoon <hobinyoon@gmail.com>
2015-04-29 11:19:08 -0400

    add remote backup

--- db/backup.sh
+++ db/backup.sh
@@ -7,14 +7,20 @@ DB_NAME=twitter3
 CUR_DATETIME=`date +%y%m%d-%H%M%S`
 echo $CUR_DATETIME
 
-FN_OUT="/mnt/data/twitter/backup/"$DB_NAME"-"$CUR_DATETIME".sql"
+FN_SQL="/mnt/data/twitter/backup/"$DB_NAME"-"$CUR_DATETIME".sql"
+FN_SQL_7Z=$FN_SQL".7z"
 
-time /usr/bin/mysqldump --skip-tz-utc -u twitter -ptwitterpass $DB_NAME > $FN_OUT
+time /usr/bin/mysqldump --skip-tz-utc -u twitter -ptwitterpass $DB_NAME > $FN_SQL
 
-/usr/bin/7z a -mx=9 $FN_OUT".7z" $FN_OUT
-\rm $FN_OUT
+/usr/bin/7z a -mx=9 $FN_SQL_7Z $FN_SQL
+\rm $FN_SQL
 
 /usr/bin/find /mnt/data/twitter/backup -type f -ls | /usr/bin/sort -k 11 -r | /usr/bin/head -n 5
+
+REMOTE_MACHINE="143.215.207.89"
+echo
+echo "Remote backup to "$REMOTE_MACHINE
+time scp -p $FN_SQL_7Z hobin@$REMOTE_MACHINE:archive/backup/twitter/
 
 echo
 echo "Strip the credential table before distribution!"

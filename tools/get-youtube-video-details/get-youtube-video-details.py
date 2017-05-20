95fc677f8e98c5c0b9886eda8976008162c9d6c2
Hobin Yoon <hobinyoon@gmail.com>
2017-05-20 12:09:15 -0400

    update

--- tools/get-youtube-video-details/get-youtube-video-details.py
+++ tools/get-youtube-video-details/get-youtube-video-details.py
@@ -11,16 +11,32 @@ import Cons
 
 import YoutubeListVideos
 
-def main(argv):
+def GetVideoDetails():
 	ids = ["Ks-_Mh1QhMc", "c0KYU2j0TM4", "eIho2S0ZahI"]
 	vlist = YoutubeListVideos.List(ids)
 	#pprint.pprint(vlist)
 
 	fn = "video-details.pkl"
 	with open(fn, "wb") as fo:
-		for i in vlist["items"]:
+		items = vlist["items"]
+		fo.write("%d\n" % len(items))
+		for i in items:
 			pickle.dump(i, fo, pickle.HIGHEST_PROTOCOL)
 	Cons.P("Created %s %d" % (fn, os.path.getsize(fn)))
+
+
+def ReadVideoDetails():
+	fn = "video-details.pkl"
+	with open(fn, "rb") as fo:
+		num_videos = int(fo.readline())
+		for i in range(num_videos):
+			v = pickle.load(fo)
+			pprint.pprint(v)
+
+
+def main(argv):
+	GetVideoDetails()
+	#ReadVideoDetails()
 
 
 if __name__ == "__main__":
